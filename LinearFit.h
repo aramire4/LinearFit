5b1e72c82dd8446e24931f921a80f16b679885c3
aramire4 <aramire4@titan13.cs.trinity.edu>
2018-11-26 15:49:45 -0600

    added some of the pseudocode

--- LinearFit.h
+++ LinearFit.h
@@ -1,9 +1,24 @@
 
 #include <algorithm>
+#include <math.h>
+#include <stdio.h>
 
 template<class T,class F>
 vector<double> fitFuncs(const vector<T> &data,const vector<F> &functions){
+    int num = data.size();
 
+    vector<vector<int>> matrix;
+    vector<int> vect(num, 0);
+    for(int i = 0; i < num; i++){
+        vect[i] = i;
+    }
+    
+    for(int k = 1; k < num; k++){
+        int p = 0;
+        for (int i = k; i < num; i++){
+
+        }
+    }
 
 
 }
