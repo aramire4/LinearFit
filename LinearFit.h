7086b43f461b216079225647d903eeea61066fd8
aramire4 <aramire4@titan13.cs.trinity.edu>
2018-11-26 16:14:49 -0600

    Possibly made the matrix correctly, need to check

--- LinearFit.h
+++ LinearFit.h
@@ -7,11 +7,21 @@ template<class T,class F>
 vector<double> fitFuncs(const vector<T> &data,const vector<F> &functions){
     int num = data.size();
 
-    vector<vector<int>> matrix;
-    vector<int> vect(num, 0);
+    //vector<int> vect(num, 0);
+    vector<vector<double>> matrix (num, vector<double>(num, 0));
+    for(int i = 0; i < num; i++){
+        for(int k = 0; k < num; k++){
+            F func = functions[k];
+            matrix[i][k] = func(data[i]);
+            //TODO-check
+        }
+    }
+
+    /*
     for(int i = 0; i < num; i++){
         vect[i] = i;
     }
+    */
     
     for(int k = 1; k < num; k++){
         int p = 0;
